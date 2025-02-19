2906a89f70628c429ac312aaf7b1f099366a5ec4
seoyoungKim <seoyoungkim003@gmail.com>
2025-02-19 23:32:27 +0900

    last upload

--- src/model/restaurant.ts
+++ src/model/restaurant.ts
@@ -22,3 +22,11 @@ export type Menu = {
 export type AddressWithoutZip = Omit<PaymentAddress, 'zipCoide'>  //address에서 zipcode만 빼고 싶을 땐 재정의가 아닌 이렇게 하면 된다.
 export type RestaurantOnlyCategory = Pick<Restaurant, 'category'> //선택한 것만 가져오고 싶을 때 
 
+export type ApiResponse<T>{
+    data:T[],
+    totalPage:number,
+    page:number
+}
+
+export type RestaurantResponse = ApiResponse<Restaurant>
+export type MenuResponse = ApiResponse<Menu>
